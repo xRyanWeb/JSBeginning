6b3e86be56a42803e27b0a2689a675ee9debc8c5
Ryan James Gregoire <ryangregoire286@gmail.com>
2025-01-03 18:12:52 -0500

    Added Full Section One of Course JavaScript

---
+++ Day-1/Syntax/BooleanValues.js
@@ -0,0 +1,10 @@
+const isAdult = true;
+let isTrue = true;
+console.log(isAdult, isTrue);
+let isFalse = false;
+console.log(isFalse);
+
+let notTrue = !true;
+let notFalse = !false
+console.log(notFalse, notTrue);
+
