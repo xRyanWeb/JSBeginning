4ce0a62fed7b0b29ce2d849ab4d157cabfd848e3
Ryan James Gregoire <ryangregoire286@gmail.com>
2025-01-03 18:10:57 -0500

    Added Full Section One of Course JavaScript

---
+++ Day-1/Syntax/Numbers.js
@@ -0,0 +1,13 @@
+let nums = 25;
+let negNum = -25;
+let dec = 25.1;
+
+console.log(nums, negNum, dec);
+
+let numNegOne = 1.5e+10;
+console.log(numNegOne);
+
+console.log("Min Value: " + Number.MIN_VALUE);
+console.log("Min Safe: " + Number.MIN_SAFE_INTEGER);
+console.log("Max Safe: " + Number.MAX_SAFE_INTEGER);
+console.log("Max Value: " + Number.MAX_VALUE);
