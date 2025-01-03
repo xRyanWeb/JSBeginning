bc63c6497829b1f5c0c6d87d7715c320fa462728
Ryan James Gregoire <ryangregoire286@gmail.com>
2025-01-03 18:22:33 -0500

    Learned About Objects

---
+++ Day-1/Syntax/Objects.js
@@ -0,0 +1,22 @@
+let object1 = {};
+
+object1.name = "Ryan";
+object1.age = 25;
+object1.isAdmin = true;
+
+object1.hasJob = true;
+
+let obj1 = {
+    firstName: "Ryan",
+    age: 25,
+    isAdmin: true,
+    lastName: "James"
+}
+
+obj1.date = new Date("11/28/2023");
+
+console.log(`Age: ${obj1.age}`)
+console.log(`Admin: ${obj1.isAdmin}`)
+console.log(`First Name: ${obj1.firstName}`)
+console.log(`Last Name: ${obj1.lastName}`)
+console.log(`Date: ${obj1.date}`)
