ba528432d69d041e88ad87491975c4a1614c9393
Ryan James Gregoire <ryangregoire286@gmail.com>
2025-01-02 23:52:01 -0500

    Learned About Classes

---
+++ index.js
@@ -0,0 +1,44 @@
+class Main {
+    a = 0;
+    b = 0;
+
+    constructor(a, b) {
+        this.a = a;
+        this.b = b;
+
+        this.getCommands = () => {
+            console.log("a: " + this.a);
+            console.log("b: " + this.b);
+
+        }
+
+        this.birthdayDate = (date) => {
+            console.log("Your Birthday is: " + date)
+        }
+
+        this.displayYearsCode = (a) => {
+            console.log("You Have Been Coding for: " + a + " Years");
+        }
+
+
+
+    }
+
+}
+
+
+let firstName = "Ryan";
+let lastName = "James";
+let fullName = firstName + " " + lastName;
+
+const myName = new Main(firstName, lastName);
+myName.getCommands();
+
+const date = new Date("11/17/1999");
+
+const myDate = new Main();
+myDate.birthdayDate(date);
+
+let yearsNow = Math.abs(2022 - 2025);
+const yearsDate = new Main();
+yearsDate.displayYearsCode(yearsNow);
