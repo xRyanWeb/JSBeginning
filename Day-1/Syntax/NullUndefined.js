59a88a52f0dd9943ce4c9895f06c39270ec6bca3
Ryan James Gregoire <ryangregoire286@gmail.com>
2025-01-03 18:16:08 -0500

    Added Undefined and null Learned

---
+++ Day-1/Syntax/NullUndefined.js
@@ -0,0 +1,13 @@
+let xy;
+
+console.log(xy);
+
+console.log(typeof(xy));
+
+xy = "Hello World";
+console.log(xy);
+
+let isUndefined = null;
+let hasManager = null
+
+console.log(hasManager, isUndefined);
